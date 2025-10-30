27046251a7ee673fa868ae218e002a8c05e84654
kaiky <kaikycintra@gmail.com>
2025-10-30 13:53:44 -0300

    feat: adds appropriate allowed host (needs to be done via environment variables)

--- projeto/projeto/settings.py
+++ projeto/projeto/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gy!z-sbd!1*$x-i9qjoo1gwslt1q891wx4&(s3zk6fdij^ynal'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
